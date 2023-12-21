955abd6db49b70a048bbc015bb1d98bea851f426
Felipe <fsoares@autodoc.com.br>
2023-12-21 17:04:36 -0300

    setting up enviroment prisma

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { app } from '@/app'
 import { authenticate } from '@/http/controllers/authenticate'
 import { register } from '@/http/controllers/register'
 import { profile } from './controllers/profile'
+import { verifyJwt } from './middleware/verify-jwt'
 
 export async function appRoutes() {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   // Authenticated
-  app.get('/me', profile)
+  app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
