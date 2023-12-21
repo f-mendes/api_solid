032c82bfd4b9f2b82fbf82868d670cb3c27cc128
Felipe <fsoares@autodoc.com.br>
2023-12-21 09:22:04 -0300

    setting up jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
 import { app } from '@/app'
 import { authenticate } from '@/http/controllers/authenticate'
 import { register } from '@/http/controllers/register'
+import { profile } from './controllers/profile'
 
 export async function appRoutes() {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.get('/me', profile)
 }
