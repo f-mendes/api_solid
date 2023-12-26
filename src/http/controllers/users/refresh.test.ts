266686c2c3e304888e17c192c5a7e00dd2ff4735
Felipe <fsoares@autodoc.com.br>
2023-12-26 17:29:25 -0300

    implement refresh token and RBAC

---
+++ src/http/controllers/users/refresh.test.ts
@@ -0,0 +1,44 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Refresh Token (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to refresh a token', async () => {
+    // @ts-expect-error: Unreachable code error
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    // @ts-expect-error: Unreachable code error
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const cookies = authResponse.get('Set-Cookie')
+
+    // @ts-expect-error: Unreachable code error
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies)
+      .send()
+
+    expect(response.status).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ])
+  })
+})
