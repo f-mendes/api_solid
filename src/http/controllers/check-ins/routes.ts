83e1ba62e0e80fab3c82be434b16fa9ecacf02bf
Felipe <fsoares@autodoc.com.br>
2023-12-26 16:12:20 -0300

    create test e2e for check-ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -12,6 +12,6 @@ export async function checkInsRoutes(app: FastifyInstance) {
   app.get('/check-ins/history', history)
   app.get('/check-ins/metrics', metrics)
 
-  app.post('/check-ins/gyms/:gymId/check-ins', create)
+  app.post('/gyms/:gymId/check-ins', create)
   app.patch('/check-ins/:checkInId/validate', validate)
 }
